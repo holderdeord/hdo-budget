e43b32ebeb25864708750191f1e55639adb96c2e
arne <megoth@ubuntu.(none)>
2013-11-27 20:45:32 +0100

    A simple webserver to run for heroku

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+app.configure(function () {
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname) //where your static content is located in your filesystem
+    );
 });
+app.listen(port); //the port you want to use
+console.log("Express server running");
