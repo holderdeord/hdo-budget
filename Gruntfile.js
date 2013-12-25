64c048e4288aac01cf87580c27ce2228fc27b56e
arne <megoth@ubuntu.(none)>
2013-12-25 18:27:26 +0100

    Added node test to test stack (with buster.js); configured travis to new setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,11 @@ module.exports = function(grunt) {
         }
       }
     },
+    buster: {
+      node: {
+
+      }
+    },
     exec: {
       web: {
         cmd: "node web.js"
@@ -47,11 +52,13 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-buster');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-karma');
 
+  grunt.registerTask('test:deployment', ['karma:travis', 'buster::test'])
   grunt.registerTask('test:browsers', ['karma:browsers']);
   grunt.registerTask('test:travis', ['karma:travis']);
   grunt.registerTask('default', ['exec:web']);
