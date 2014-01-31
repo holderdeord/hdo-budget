82f47d9a97aa56fc6fc0f418f15cffa62c177be8
arne <megoth@ubuntu.(none)>
2014-01-31 18:09:06 +0100

    First attempt at merging MDG alternativ budsjett with statsbudsjettet

--- combinePost.js
+++ combinePost.js
@@ -8,15 +8,30 @@ var budgetFactory = require("./src/budgetFactory");
 var util = require("util");
 
 program
+  .option('-m, --multiply [value]', 'Number to multiply with (integer)')
   .option('-o, --output [value]', 'Name of file to output csv to; if none given will output to console')
+  .option('-f, --flip [type]', 'Type of posts to flip (revenue|cost)')
   .parse(process.argv);
 
 var budget = budgetFactory.$new(Q);
+var multiply = program.multiply || 1;
+var flipRevenue = program.flip && program.flip == 'revenue' ? -1 : 1;
+var flipCost = program.flip && program.flip == 'cost' ? -1 : 1;
+var isRevenue = function (chapterNo) {
+  return chapterNo > 3000;
+}
 var promises = program.args.map(function (path) {
   var deferred = Q.defer();
+  var amount = 0;
+  var flip = 1;
   fs.readFile(path, 'utf-8', function (err, data) {
     var d = d3.csv.parse(data).forEach(function (p) {
-      budget.addPost(p.chapterNo, p.postNo, p.text, parseInt(+p.amount.replace(/\s/g, "")));
+      amount = p.amount.replace(/(-−)?\s+/g, "");
+      multiply *= (amount[0] == '−' || amount[0] == '-') ? -1 : 1;
+      flip = isRevenue(p.chapterNo) ? flipRevenue : flipCost;
+      amount = (amount[0] == '−' || amount[0] == '-') ? amount.substr(1) : amount;
+      amount = parseInt(amount) * multiply * flip;
+      budget.addPost(p.chapterNo, p.postNo, p.text, amount);
     });
     deferred.resolve(d);
   });
