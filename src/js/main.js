1290a092a4120fa402494cbcdd93d85908218077
Jari Bakken <jari@holderdeord.no>
2015-07-11 18:15:01 +0200

    Add eslint

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import '../css/main.scss';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <h1>Hei budsjett!</h1>
-    );
-  }
+    render() {
+        return (
+            <h1>Hei budsjett!</h1>
+        );
+    }
 }
 
 React.render(<App />, document.getElementById('root'));
